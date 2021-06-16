f87b1390cd5546327e9d932934ff639ce490e724
mistermasternimish <81861117+mistermasternimish@users.noreply.github.com>
2021-06-16 13:30:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -75,15 +75,27 @@ function draw() {
  
  
 }
-
 function keyPressed() {
- if (keyCode == DOWN_ARROW) {
-	 console.log("down")
-	Matter.Body.setStatic(packageBody, false);
-	   
-	
+  if (keyCode === LEFT_ARROW) {
+
+    helicopterSprite.x=helicopterSprite.x-20;    
+    translation={x:-20,y:0}
+    Matter.Body.translate(packageBody, translation)
+
+
+  } else if (keyCode === RIGHT_ARROW) {
+    helicopterSprite.x=helicopterSprite.x+20;
+    translation={x:20,y:0}
+    Matter.Body.translate(packageBody, translation)
+  }
+  else if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
   }
 }
+
+
+
 
 
 
